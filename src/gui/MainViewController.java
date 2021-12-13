5b8465ede029e631f564738d55c99c9c75a72cb1
Aliny Kelly <alinykellyfs@gmail.com>
2021-12-13 13:03:07 -0300

    SellerList

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -17,6 +17,7 @@ import javafx.scene.control.MenuItem;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
 import model.services.DepartmentService;
+import model.services.SellerService;
 
 public class MainViewController implements Initializable{
 	
@@ -34,12 +35,14 @@ public class MainViewController implements Initializable{
 	// Metodos para tratar os eventos do Menu
 	@FXML
 	public void onMenuItemSellersAction() {
-		System.out.println("onMenuItemSellersAction");
+		loadView("/gui/SellerList.fxml", (SellerListController controller) -> {
+			controller.setSellerService(new SellerService());
+			controller.updateTableView();
+		});//expressao lambda
 	}
 	
 	@FXML
-	public void onMenuItemDepartmentAction() {
-		
+	public void onMenuItemDepartmentAction() {		
 		loadView("/gui/DepartmentList.fxml", (DepartmentListController controller) -> {
 			controller.setDepartmentService(new DepartmentService());
 			controller.updateTableView();
