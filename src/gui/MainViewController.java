c9fc8d1cf929f50f8dc5a7b3e4fc697abbb87e44
Aliny Kelly <alinykellyfs@gmail.com>
2021-12-12 21:44:07 -0300

    Tela Sobre
    Implementacao da tela sobre

---
+++ src/gui/MainViewController.java
@@ -0,0 +1,76 @@
+package gui;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.Main;
+import gui.util.Alerts;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
+
+public class MainViewController implements Initializable{
+	
+	//Atributos dos itens do Menu
+	@FXML
+	private MenuItem menuItemVendedores;
+	
+	@FXML
+	private MenuItem menuItemDepartamento;
+	
+	@FXML
+	private MenuItem menuItemSobre;
+	
+	
+	// Metodos para tratar os eventos do Menu
+	@FXML
+	public void onMenuItemVendedoresAction() {
+		System.out.println("onMenuItemVendedoresAction");
+	}
+	
+	@FXML
+	public void onMenuItemDepartamentoAction() {
+		System.out.println("onMenuItemDepartamentoAction");
+	}
+	
+	@FXML
+	public void onMenuItemSobreAction() {
+		loadView("/gui/Sobre.fxml");
+	}
+	
+	
+	//Metodo da interface Initializable
+	@Override
+	public void initialize(URL uri, ResourceBundle rb) {
+		
+	}
+	
+	//Função para abrir outra tela
+	private synchronized void loadView(String absoluteName) {
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox = (VBox) ((ScrollPane) mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			
+		}
+		catch(IOException e) {
+			Alerts.showAlert("IO Exception", "Erro ao carregar a página", e.getMessage(), AlertType.ERROR);
+		}
+	}
+
+}
